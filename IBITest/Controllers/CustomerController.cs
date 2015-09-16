2a69127c3911b52e2d75a354cc6c07a418860b4e
akale <akale@visa.com>
2015-09-16 19:35:25 +0530

    fadji pull

--- IBITest/Controllers/CustomerController.cs
+++ IBITest/Controllers/CustomerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Windows.Forms;
 namespace IBITest.Controllers
 {
     public class CustomerController : Controller
@@ -15,8 +15,9 @@ namespace IBITest.Controllers
         {
             return View();
         }
-        public ActionResult FinishRegistration()
+        public ActionResult FinishRegistration(string token)
         {
+            MessageBox.Show(token);
             return View();
         }
 
