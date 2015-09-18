aa1dd9fb7b49858c11d294ccde0603fa1f1abeee
akale <akale@visa.com>
2015-09-18 20:02:14 +0530

    admin request getting view complete

--- IBITest/Controllers/AdminController.cs
+++ IBITest/Controllers/AdminController.cs
@@ -63,6 +63,18 @@ namespace IBITest.Controllers
             //return Json(obj);
             return Json(obj);
         }
+        public JsonResult GetClosureOfAccountAdminRequest(string requestType)
+        {
+            //Console.WriteLine("some one called me");
+            //Console.ReadKey();
+            //MessageBox.Show("some one called me");
+            List<ClosureOfAccountAdminView> obj = new List<ClosureOfAccountAdminView>(){
+                new ClosureOfAccountAdminView() {requestID=1,customerID=1,accountNumber=1},
+                new ClosureOfAccountAdminView() { requestID=2,customerID=2,accountNumber=2}
+            };
+            //return Json(obj);
+            return Json(obj);
+        }
         public void Approve(List<int> approveRequestList)
         {
             //MessageBox.Show("In approve method");
