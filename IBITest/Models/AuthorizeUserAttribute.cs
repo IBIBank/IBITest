e8e91e12dd378f02d4990757fbbc321eccea2e1e
Som Madhab Bhattacharya <smb.iitkgp@gmail.com>
2015-09-23 12:05:57 +0530

    commit

---
+++ IBITest/Models/AuthorizeUserAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IBITest.Models
+{
+    public class AuthorizeUserAttribute : AuthorizeAttribute
+    {
+        public string AccessLevel { get; set; }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            var isAuthorized = base.AuthorizeCore(httpContext);
+            if (!isAuthorized)
+            {
+                return false;
+            }
+
+            string privilegeLevels = "Admin"; // Call another method to get rights of the user from DB
+
+            if (privilegeLevels.Contains(this.AccessLevel))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
