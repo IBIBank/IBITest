47c4d978b163d138460d63bac9a6bbd3d5c03344
akale <akale@visa.com>
2015-09-26 21:49:33 +0530

    aaply loan changes

--- IBITest/Models/LoanRequestViewModel.cs
+++ IBITest/Models/LoanRequestViewModel.cs
@@ -3,28 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel;
 namespace IBITest.Models
 {
     public class LoanRequestViewModel
     {
+        [DisplayName("Customer ID")]
         public long customerID { get; set; }
         [Range(21,150)]
+        [DisplayName("Age")]
         public int age { get; set; }
         [Required]
-        [Range(50000,Double.MaxValue)]
+        [Range(50000,Double.MaxValue, ErrorMessage="Annual Income must be minimum 50000")]
+        [DisplayName("Annual Income")]
         public Decimal annualIncome { get; set; }
         [Required]
+        [DisplayName("Type of Loan")]
         public char typeOfLoan { get; set; }
         [Required]
+        [DisplayName("Amount")]
         public Decimal amount { get; set; }
         [Required]
+        [DisplayName("Tenure ( in months)")]
         public Decimal tenure { get; set; }
         [Required]
+        [DisplayName("Branch Name")]
         public string branchName { get; set; }
         [Required]
+        [DisplayName("Salary Proof")]
         public Byte[] salaryProof { get; set; }
         [Required]
+        [DisplayName("Address Proof")]
         public Byte[] addressProof { get; set; }
     }
 }
