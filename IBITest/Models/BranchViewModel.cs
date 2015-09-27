b582d5da72886435e5b58859c7a723fec7ab878d
Som Madhab Bhattacharya <smb.iitkgp@gmail.com>
2015-09-27 16:53:12 +0530

    Added GetAllBranch

---
+++ IBITest/Models/BranchViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IBITest.Models
+{
+    public class BranchViewModel
+    {
+        public string branchCode { get; set; }
+        public string branchName { get; set; }
+        public string cityName { get; set; }
+        public string address { get; set; }
+        public string contactNumber { get; set; }
+        public string bankerName { get; set; }
+        public string branchLogInID { get; set; }
+        public string branchPassword { get; set; }
+        public string email { get; set; }
+    }
+
+}
